6255cb4bdf037b143e94803dcc7cfa9d274a1348
mdoyle91 <mike.w.doyle91@gmail.com>
2024-10-01 12:47:38 -0400

    HTML & JS data populating to corresponding areas

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
+//setters
 const textInput = document.querySelector(`#searchBar`)
 const searchButton = document.querySelector(`#searchButton`)
 const imgContainer = document.querySelector(`#imgContainer`)
+const id = document.querySelector(`#objectID`)
+const yearAcquired = document.querySelector(`#accessionYear`)
+const objectDepartment = document.querySelector(`#department`)
+const type = document.querySelector(`#objectName`)
+const objectTitle = document.querySelector(`#title`)
+const objectCulture = document.querySelector(`#culture`)
+const artist = document.querySelector(`#artistDisplayName`)
+const bio = document.querySelector(`#artistDisplayBio`)
+const dateCreated = document.querySelector(`#objectDate`)
+const objectMedium = document.querySelector(`#medium`)
+const objectDimensions = document.querySelector(`#dimensions`)
+const source= document.querySelector(`#creditLine`)
+const copyright= document.querySelector(`#rightsAndReproduction`)
+const site= document.querySelector(`#linkResource`)
+const objectTags= document.querySelector(`#tags`)
+const objectGalleryNumber = document.querySelector(`#GalleryNumber`)
+
 
 searchButton.addEventListener(`click`, async ()=> { //For DRY code, may want to separate click event listener from overall function if necessary
     let searchText= textInput.value
     console.log(searchText)
     let response = await axios.get(`
         https://collectionapi.metmuseum.org/public/collection/v1/objects/${searchText}`)
-    console.log(response)
     let objectImage = response.data.primaryImage
-    console.log(objectImage)
     imgContainer.setAttribute("src", objectImage)
-    let {objectID, accessionYear, department, objectName, title, culture, artistDisplayName, atristDisplayBio, artistBeginDate, artistEndDate, objectDate, medium, dimensions, creditLine, rightsAndReproduction, linkResource, tags, GalleryNumber} = response.data
-    console.log(objectID)
-    console.log(accessionYear)
+    let {objectID, accessionYear, department, objectName, title, culture, artistDisplayName, atristDisplayBio, objectDate, medium, dimensions, creditLine, rightsAndReproduction, linkResource, tags, GalleryNumber} = response.data
+    id.textContent = `Object ID: ${objectID}`
+    yearAcquired.textContent = `Year Acquired: ${accessionYear}`
+    objectDepartment.textContent = `Department: ${department}`
+    type.textContent= `Object Type: ${objectName}`
+    objectTitle.textContent = `Title: ${title}`
+    objectCulture.textContent = `Culture: ${culture}`
+    artist.textContent = `Artist: ${artistDisplayName}`
+    bio.textContent = `Bio: ${atristDisplayBio}`
+    dateCreated.textContent = `Object Created: ${objectDate}`
+    objectMedium.textContent = `Medium: ${medium}`
+    objectDimensions.textContent = `Dimensions: ${dimensions}`
+    source.textContent = `Source of Artwork: ${creditLine}`
+    copyright.textContent = `Copyright: ${rightsAndReproduction}`
+    site.textContent = `Site Info: ${linkResource}`
+    objectTags.textContent = `Tags: ${tags}`
+    objectGalleryNumber.textContent = `Gallery Number: ${GalleryNumber}`
 })
+
 
 
 
